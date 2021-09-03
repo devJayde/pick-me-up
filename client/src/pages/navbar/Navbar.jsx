afb13523fb35f7261fd51a0bc578d9efdbf4d6dc
devjade <jiyoung.park98@gmail.com>
2021-09-03 13:13:19 +0900

    FEAT/design

--- client/src/pages/navbar/Navbar.jsx
+++ client/src/pages/navbar/Navbar.jsx
@@ -40,11 +40,11 @@ const Navbar = ({
 
   const handleFollow = () => {
     setScrollY(window.pageYOffset);
-    if(ScrollY > 100) {
-      // 100 이상이면 버튼이 보이게
+    if(ScrollY > 400) {
+      // 400 이상이면 버튼이 보이게
       setBtnStatus(true);
     } else {
-      // 100 이하면 버튼이 사라지게
+      // 400 이하면 버튼이 사라지게
       setBtnStatus(false);
     }
   }
